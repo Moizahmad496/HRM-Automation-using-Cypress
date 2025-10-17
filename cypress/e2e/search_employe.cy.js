2f787839022ce1bc3d44f643339a84fbc008fa34
Moizahmad496 <moizahmad248@gmail.com>
2025-10-17 10:20:03 +0500

    Pipeline integration check

--- cypress/e2e/search_employe.cy.js
+++ cypress/e2e/search_employe.cy.js
@@ -16,9 +16,9 @@ describe("OrangeHRM PIM Scenarios", () => {
     cy.url().should("include", "/pim/viewEmployeeList");
 
     // Search Employee by Information
-    cy.get('input[placeholder="Type for hints..."]').first().type("Test");
+    cy.get('input[placeholder="Type for hints..."]').first().type("Testing the applicatio ");
     cy.get(".oxd-select-text-input").first().click();
-    cy.contains(".oxd-select-option", "Full-Time Permanent").click();
+    cy.contains(".oxd-select-option", "Full-Time Permanent").click(); 
     //Choosing the include field
     cy.get(".oxd-select-wrapper").eq(1).click();
     cy.contains(".oxd-select-option", "Current Employees Only").click();
